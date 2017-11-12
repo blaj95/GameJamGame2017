f6679af4d5e2452e6aeff34af37967fe378e39fd
Ian MacKenzie <32595467+Shoebopadoo@users.noreply.github.com>
2017-11-11 21:23:00 -0500

    Drunk Filter V1
    Made the start of our beer googles

---
+++ Assets/PostProcessing/Effects.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class Effects : MonoBehaviour {
+
+    public PostProcessingProfile my_Profile;
+
+
+    public float quantitativeDrunk = 0f;
+    public float shutterModifier = 0f;
+
+
+
+
+    // Use this for initialization
+    void OnEnable ()
+    {
+        //var behaviour = GetComponent<PostProcessingBehaviour>();
+
+        //if (behaviour.profile = null)
+        //{
+        //    enabled = false;
+        //    return;
+        //}
+
+        //my_Profile = Instantiate(behaviour.profile);
+        //behaviour.profile = my_Profile;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            var vignette = my_Profile.vignette.settings;
+            var motionBlur = my_Profile.motionBlur.settings;
+
+            motionBlur.frameBlending += quantitativeDrunk;
+            motionBlur.shutterAngle += shutterModifier;
+
+            vignette.intensity += quantitativeDrunk;
+
+            my_Profile.motionBlur.settings = motionBlur;
+            my_Profile.vignette.settings = vignette;
+        }
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            var vignette = my_Profile.vignette.settings;
+            var motionBlur = my_Profile.motionBlur.settings;
+
+            motionBlur.frameBlending = 0;
+            motionBlur.shutterAngle = 0;
+            vignette.intensity = 0;
+
+            my_Profile.motionBlur.settings = motionBlur;
+            my_Profile.vignette.settings = vignette;
+
+
+        }
+        
+       
+        //motionBlur.frameBlending = Mathf.Abs((Mathf.Sin(Time.realtimeSinceStartup) * 0.99f) + 0.01f);
+        
+	}
+}
